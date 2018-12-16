c2efc512f9338f1170ae526e385e7c22b7cc9e38
Szymon <krasuski.szymon.piotr@gmail.com>
2018-12-16 15:21:34 +0100

    Working listener, example .csv

--- run_listener.py
+++ run_listener.py
@@ -43,7 +43,7 @@ class MouseListener:
 
     def on_click_mouse(self, x ,y ,button, pressed):
         print(button)
-        if pressed:
+        if not pressed:
             self.recorder.append_data(button)
 
     def on_scroll_mouse(self, x, y, dx, dy):
@@ -64,9 +64,9 @@ class DataSaver:
     def save_data(self, date_time):
         date = str(date_time.date())
         time = date_time.strftime('%d-%m-%Y %H:%M:%S')
-        with open('data/typer_{date}'.format(date=date), 'a') as log_file:
+        with open('data/typer_{date}.csv'.format(date=date), 'a') as log_file:
             csv_writer = csv.writer(log_file, delimiter=',')
-            data = self.recorder.get_data()
+            data = self.recorder.get_data().copy()
             for key, value in data.iteritems():
                 csv_writer.writerow([time, key, value])
         print('Data saved to typer_{date} for {time}'.format(date=date, time=time))
@@ -88,6 +88,17 @@ if __name__ == '__main__':
     thread_keylistener = threading.Thread(target=key_listener.listen)
     thread_mouse_listener = threading.Thread(target=mouse_listener.listen)
     thread_saver = threading.Thread(target=saver.check_for_saving)
+    thread_keylistener.setDaemon(True)
+    thread_mouse_listener.setDaemon(True)
+    thread_saver.setDaemon(True)
     thread_keylistener.start()
     thread_mouse_listener.start()
     thread_saver.start()
+    while True:
+        if not thread_saver.is_alive():
+            thread_saver.start()
+        if not thread_keylistener.is_alive():
+            thread_keylistener.start()
+        if not thread_mouse_listener.is_alive():
+            thread_mouse_listener.start()
+        pass
